684076b6fad0a54e79426b4a71b143f2062a7325
leonawolff <leona.wolff.ok@gmail.com>
2022-04-08 00:28:48 +0100

    Updating approver fields correctly

--- src/components/Approval.vue
+++ src/components/Approval.vue
@@ -8,11 +8,11 @@
         <button v-on:click="onClick(false)">Reject</button>
       </ul>
     </div>
-    <div v-if="clicked&&success">
+    <div v-if="clicked && success">
       <h4>Success!</h4>
       <p>Your response has been recorded. Thanks!</p>
     </div>
-    <div v-if="clicked&&failure">
+    <div v-if="clicked && failure">
       <h4>Uh oh!</h4>
       <p>Something went wrong. Please approve/deny this booking via our app!</p>
     </div>
@@ -21,67 +21,96 @@
 
 <script>
 import { firebase } from "../firebaseInit";
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import { getDoc } from "firebase/firestore";
 
 export default {
-  name: 'Approval',
+  name: "Approval",
   props: {
-      bookingRef: String,
-      userEmail: String,
-      userID: String
+    bookingRef: String,
+    userEmail: String,
+    userID: String,
   },
-  data (){
+  data() {
     return {
       success: false,
       failure: false,
-      clicked:false
-    }
+      clicked: false,
+      approverFound: false
+    };
   },
   methods: {
+    async UpdateBooking(propData) {
+      const updateMsg = propData.updateMsg;
+      const booking = propData.booking;
+      const userEmail = propData.user;
 
-  async UpdateBooking(propData) {
-    
-    const update = propData.update
-    const booking = propData.booking
-    console.log(update + " booking " + booking)
-    const entityRef = firebase.firestore().collection('bookings').doc(booking);
-    entityRef
-        .update({status:update})
+      console.log(updateMsg + " booking " + booking);
+      const entityRef = firebase
+        .firestore()
+        .collection("bookings")
+        .doc(booking);
+
+
+      const docSnap = await getDoc(entityRef);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+      } else {
+        console.log("No such document!");
+      }
+      let approverList = docSnap.data().approvers;
+      var size = Object.keys(approverList).length;
+      console.log(size);
+
+      for(const key of Object.getOwnPropertyNames(approverList)){
+        console.log(key)
+        console.log(userEmail)
+        if(key == userEmail){
+          this.approverFound = true;
+        }
+      }
+      if (this.approverFound){
+        approverList[userEmail] = updateMsg;
+      }
+
+      entityRef
+        .update({
+          approvers: approverList
+        })
         .then(() => {
-            console.log('Booking updated!');
-            this.success = true
-          })
-        .catch(error => {
-            console.log(error)
-            this.failure = true
+          console.log("Booking updated!");
+          this.success = true;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.failure = true;
         });
-},
-    approveBooking () {
-      var propData = {
-          booking:this.bookingRef,
-          update:"Approved"
-      }
-      this.UpdateBooking(propData)
     },
-    denyBooking () {
+    approveBooking() {
       var propData = {
-          booking:this.bookingRef,
-          update:"Denied"
-      }
-      this.UpdateBooking(propData)
+        booking: this.bookingRef,
+        user: this.userEmail,
+        updateMsg: "Approved",
+      };
+      this.UpdateBooking(propData);
+    },
+    denyBooking() {
+      var propData = {
+        booking: this.bookingRef,
+        user: this.userEmail,
+        updateMsg: "Denied",
+      };
+      this.UpdateBooking(propData);
     },
 
-    onClick(approve){
-      this.clicked=true;
-      if(approve)
-        this.approveBooking();
-      else
-        this.denyBooking();
-    }
-  }
-}
-
+    onClick(approve) {
+      this.clicked = true;
+      if (approve) this.approveBooking();
+      else this.denyBooking();
+    },
+  },
+};
 </script>
 
 <style scoped>
