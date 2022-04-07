19fa32638b6aa331afbc9e9e3a0584b804c047be
leonawolff <leona.wolff.ok@gmail.com>
2022-04-07 17:40:40 +0100

    Update working

--- src/components/Approval.vue
+++ src/components/Approval.vue
@@ -26,73 +26,75 @@ export default {
     return {
       response: Boolean,
       result: Boolean,
+      allData: Object
     }
   },
   methods: {
-    readUser(){
-      let response = firebase.firestore()
-          .collection('users')
-          .doc(this.userID)
-          .get()
-          .then()
-          .catch(error => {
-              console.log(error)
-          });
-          console.log(response)
-  },
-    readBooking(){
-      const collection = firebase.firestore().collection('bookings')
-      const userID = this.props.userID;
-      const userType = "admin"
-      const timestamp = firebase.firestore.Timestamp.now()
-      console.log(timestamp)
-      console.log(userType)
-      if (userType == "admin"){
-          collection
-          .where("open", "==", true)
-          .where("dateOfEvent", ">", timestamp)
-          .onSnapshot(
-              querySnapshot => {
-                  const newEntities = []
-                  querySnapshot.forEach(doc => {
-                      const entity = doc.data()
-                      entity.id = doc.id
-                      newEntities.push(entity)
-                  });
-                  this.props.setEntities(newEntities)
-              },
-              error => {
-                  console.log('Error : ', error)
-              }
-          )
+  //   async readUser(){
+  //     let response = await firebase.firestore()
+  //       .collection('users')
+  //       .doc(this.userID)
+  //       .get()
+  //       .catch(error => {
+  //         console.log(error)
+  //       });
+  //     console.log(response.data)
+  // },
+
+  async UpdateBooking(propData) {
+    
+    const update = propData.update
+    const booking = propData.booking
+    console.log(update + " booking " + booking)
+    const entityRef = firebase.firestore().collection('bookings').doc(booking);
+    entityRef
+        .update({status:update})
+        .then(() => {
+            console.log('Booking updated!');
+          })
+        .catch(error => {
+            console.log(error)
+        });
+},
+    approveBooking () {
+      var propData = {
+          booking:this.bookingRef,
+          update:"Approved"
       }
-      else {
-          collection
-          .where("open", "==", true)
-          .where("tcdEmail", "==", userID)
-          .where("dateOfEvent", ">", timestamp)
-          .onSnapshot(
-              querySnapshot => {
-                  const newEntities = []
-                  querySnapshot.forEach(doc => {
-                      const entity = doc.data()
-                      entity.id = doc.id
-                      newEntities.push(entity)
-                  });
-                  this.props.setEntities(newEntities)
-              },
-              error => {
-                  console.log('Error : ', error)
-              }
-          )
-      }
+      this.UpdateBooking(propData)
     },
+    denyBooking () {
+      var propData = {
+          booking:this.bookingRef,
+          update:"Denied"
+      }
+      this.UpdateBooking(propData)
+    },
+
+    // async readBooking(){
+    //   const timestamp = firebase.firestore.Timestamp.now()
+    //   console.log(timestamp)
+    //   let response = await firebase.firestore().collection('bookings')
+    //       .where("open", "==", true)
+    //       .where("dateOfEvent", ">", timestamp)
+    //       .get()
+    //       .then(querySnapshot => {
+    //         const documents = querySnapshot.docs.map(doc => doc.data())
+    //         this.allData = documents;
+    //           error => {
+    //               console.log('Error : ', error)
+    //           }
+    //       })
+    //       console.log(response)
+    // },
+
     onClick(approve){
-      this.response = approve;
-      console.log(this.response);
-      this.readUser();
+      if(approve)
+        this.approveBooking();
+      else
+        this.denyBooking();
     }
- }
+  }
 }
 
 </script>
